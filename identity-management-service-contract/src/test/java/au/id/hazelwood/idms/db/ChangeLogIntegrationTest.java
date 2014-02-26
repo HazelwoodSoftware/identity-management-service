4a8c8cc9888a1dfdd51d0fb621f67305d5da146a
Ricky Hazelwood <rjhazelwood@gmail.com>
2014-02-26 23:43:57 +1100

    Added contract module and initial User model

---
+++ identity-management-service-contract/src/test/java/au/id/hazelwood/idms/db/ChangeLogIntegrationTest.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.id.hazelwood.idms.db;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+@FixMethodOrder(MethodSorters.JVM)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/application-context-test-changelog.xml"})
+public class ChangeLogIntegrationTest
+{
+    @Resource
+    private Object sessionFactory;
+
+    @Test
+    public void checkChangeLogUpToDate() throws Exception
+    {
+        assertThat(sessionFactory, notNullValue());
+    }
+}
