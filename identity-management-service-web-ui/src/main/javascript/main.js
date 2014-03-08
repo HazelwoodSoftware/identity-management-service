6f9bb3bce4d492a1e547b7fd0854882f4cc43ac8
Ricky Hazelwood <rjhazelwood@gmail.com>
2014-03-08 12:19:22 +1100

    Added web ui module

---
+++ identity-management-service-web-ui/src/main/javascript/main.js
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the 'License'); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+require.config({
+    paths: {
+        'require-text': [
+            '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.10/text.min',
+            'lib/requirejs/require-text'
+        ],
+        'jquery': [
+            '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min',
+            'lib/jquery/jquery'
+        ],
+        'bootstrap': [
+            '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min',
+            'lib/bootstrap/bootstrap'
+        ],
+        'angular': [
+            'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular.min',
+            'lib/angularjs/angular'
+        ],
+        'angular-route': [
+            'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular-route.min',
+            'lib/angularjs/angular-route'
+        ],
+        'angular-resource': [
+            'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular-resource.min',
+            'lib/angularjs/angular-resource'
+        ]
+    },
+    shim: {
+        'bootstrap': ['jquery'],
+        'angular': {exports: 'angular'},
+        'angular-route': ['angular'],
+        'angular-resource': ['angular']
+    },
+    priority: [
+        'jquery',
+        'bootstrap',
+        'angular'
+    ]
+});
+
+//http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
+window.name = 'NG_DEFER_BOOTSTRAP!';
+
+require(['angular', 'idms/app'], function (angular, app)
+{
+    'use strict';
+
+    angular.element().ready(function ()
+    {
+        angular.resumeBootstrap([app.name]);
+    });
+});
---
+++ identity-management-service-web-ui/src/test/javascript/idms/directives-specs.js
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+define([ 'angular', 'angular-mocks', 'idms/app' ], function (angular, mocks)
+{
+    'use strict';
+
+    describe('directives', function ()
+    {
+        beforeEach(mocks.module('idms.directives'));
+
+        describe('app-version', function ()
+        {
+            it('should print current version', function ()
+            {
+                mocks.module(function ($provide)
+                {
+                    $provide.value('version', 'TEST_VER');
+                });
+                mocks.inject(function ($compile, $rootScope)
+                {
+                    var element = $compile('<span app-version></span>')($rootScope);
+                    expect(element.text()).toEqual('TEST_VER');
+                });
+            });
+        });
+    });
+});
